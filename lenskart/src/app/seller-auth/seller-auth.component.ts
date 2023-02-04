12f403c99a96738771bb8ab83ea9d87235da1dd0
shawroshan764 <shawroshan764@gmail.com>
2023-02-04 09:16:42 +0530

    header and registration page completed

---
+++ lenskart/src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  signUp(data:object):void{
+    console.log(data);
+    
+  }
+
+}
